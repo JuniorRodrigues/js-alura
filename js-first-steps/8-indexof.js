44941dab61060a9806856b8fc4395a0fafb7d054
Junior Rodrigues <junior.frontend@gmail.com>
2021-07-01 21:01:34 -0300

    indexof

---
+++ js-first-steps/8-indexof.js
@@ -0,0 +1,50 @@
+console.log('Loops');
+
+(() => {
+    const destinosDisponiveis = [
+        'Bonito',
+        'Recife',
+        'Curitiba',
+    ];
+
+    Array.prototype.exibeLista = function () {
+        const regex = /\,\s(\w+$)/g;
+        return this.join(', ').replace(regex, ' e $1.');
+    }
+
+    const idadeMinima  = 18;
+
+    const exibirDestinos = function () {
+        console.log(`Destinos disponíveis: ${destinosDisponiveis.exibeLista()}`)
+    }
+
+    const comprarPacote = function () {
+        let mensagem;
+        const codDestino = destinosDisponiveis.indexOf(comprador.nomeDestino);
+        const podeComprar = comprador.idade >= idadeMinima || comprador.acompanhadoPorAdulto;
+
+        if (codDestino >= 0) {
+            if (podeComprar) {
+                destinosDisponiveis.splice(codDestino, 1);
+
+                mensagem = (comprador.temPassagem)
+                    ? 'Passagem já adquirida. Aproveite sua viagem!'
+                    : `Passagem para ${comprador.nomeDestino} adquirida com sucesso. Tenha uma boa viagem!`
+                ;
+            } else mensagem = 'As passagens só podem ser vendidas para maiores de idade.';
+        } else mensagem = 'Destino não disponível';
+
+        console.log(mensagem)
+    }
+
+    const comprador = {
+        nome: 'João Augusto',
+        idade: 15,
+        acompanhadoPorAdulto: true,
+        nomeDestino: 'Bonito',
+        temPassagem: false,
+    };
+
+    comprarPacote();
+    exibirDestinos();
+})();
